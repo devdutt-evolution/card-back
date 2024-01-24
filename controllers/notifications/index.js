44c44da299d82e5b560764720fbf782e383854ed
devdutt-evolution <devdutt.c@evolutioncloud.in>
2024-01-24 15:49:24 +0530

    pagination in notification
    - also added swagger defination for the same

--- controllers/notifications/index.js
+++ controllers/notifications/index.js
@@ -2,16 +2,21 @@ const { Notification } = require("../../models/notification");
 
 exports.getNotifications = async (req, res) => {
   try {
+    let { _page, _limit } = req.query;
     const userId = req.userId;
 
+    if (!_page) _page = 1;
+    else _page = parseInt(_page);
+    if (!_limit) _limit = 10;
+    else _limit = parseInt(_limit);
+
     const notifications = await Notification.find(
-      {
-        userId,
-        seen: false,
-      },
+      { userId },
       { createdAt: 0, updatedAt: 0, userId: 0 }
     )
       .sort({ createdAt: -1 })
+      .skip((_page - 1) * _limit)
+      .limit(_limit)
       .lean();
 
     res.status(200).json({ notifications });
--- swagger/notification.js
+++ swagger/notification.js
@@ -4,6 +4,17 @@
  *  get:
  *     tags:
  *     - Notifications
+ *     parameters:
+ *     - in: query
+ *       name: _page
+ *       schema:
+ *         type: number
+ *       description: for pagination
+ *     - in: query
+ *       name: _limit
+ *       schema:
+ *         type: number
+ *       description: for pagination
  *     security:
  *     - bearerAuth: []
  *     summary: Notifications list
